a61db000040bc9f72fc791ba2886167af61a027d
Marcel Rico <personal.mr4@gmail.com>
2020-01-11 03:54:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,62 @@
 class Owner
-  # code goes here
+  
+  @@all = []
+  
+  attr_reader :name,:species
+  
+  def initialize(owner_name)
+    @name = owner_name  
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+  
+  def cats
+    Cat.all.select{|cat| cat.owner == self}
+  end
+  
+  def dogs
+    Dog.all.select{|dog| dog.owner == self}
+  end
+  
+  def buy_cat(cat_name)
+    Cat.new(cat_name,self)
+  end
+  
+  def buy_dog(dog_name)
+    Dog.new(dog_name,self)
+  end
+  
+  def walk_dogs
+    Dog.all.map{|dog| dog.mood = "happy"}
+  end
+  
+  def feed_cats
+    Cat.all.map{|cat| cat.mood = "happy"}
+  end
+  
+  def sell_pets
+    Dog.all.select{|dog| dog.owner == self}.map do |dog| 
+      dog.mood = "nervous"
+      dog.owner
+    end
+      
+    Cat.all.select{|cat| cat.owner == self}.map{|cat| cat.mood = "nervous"}
+  end
+  
 end
